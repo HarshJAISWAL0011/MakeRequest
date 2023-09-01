225f44ed2bd5f22dfe77352c15b160f0b6b43d91
harsh <harshjaiswal9450@gmail.com>
2023-09-01 23:08:11 +0530

    1st

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express'; 
+import fetch from 'node-fetch'; 
+import { sendEmail } from './Email.js'; 
+const app = express();
+const baseurl='https://coin-dcx.onrender.com/';
+
+app.get('/', function(req, res) {
+    res.status(200).send({status: 200});
+});
+
+setInterval(keepServerAlive, 14*60*1000); //Make request in every 14 minutes
+// keepServerAlive();
+
+ async function keepServerAlive() {
+//render will stop the server if no request is received in 15 min.
+    fetch( baseurl )
+    .then(response => {
+        if (response.ok) {
+          console.log(`URL ${baseurl} is reachable. Status Code: ${response.status}`);
+        } else {
+          console.error(`URL ${baseurl} returned an error. Status Code: ${response.status}`);
+          sendEmail(`${baseurl} have some Error. Status Code: ${response.status}`);
+        }
+      })
+      .catch(error => {
+        console.error(`Error while checking URL ${baseurl}: ${error.message}`);
+      });
+}
