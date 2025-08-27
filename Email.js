ee68484e9a1fb368050c16e0e126493702e9e900
Harsh Jaiswal <99492891+HarshJAISWAL0011@users.noreply.github.com>
2025-08-27 20:32:37 +0530

    Update Email.js

--- Email.js
+++ Email.js
@@ -2,16 +2,34 @@ import fetch from 'node-fetch';
 
 
 export async function sendEmail (message){
+// const data = {
+//     service_id: 'Coindcx alert', 
+//     template_id: 'template_rpcqp5h',
+//     user_id: 'C1wF5QXy4IUHVBrql',
+//     template_params: {
+//         to_name: 'Harsh',
+//         message: message
+        
+//     },
+//     accessToken:'O342h4LQQ7DlYj1EaBP8K'
+// };
+// second account
+
+let service_id = 'service_7he2dof'
+let template_id =  'template_9irwoio'
+let user_id = '5g-mymygD04J2rc1o'
+let accessToken = 'sr8Y2E8P-N12Wl-Kn6x-k'
+    
 const data = {
-    service_id: 'Coindcx alert', 
-    template_id: 'template_rpcqp5h',
-    user_id: 'C1wF5QXy4IUHVBrql',
+   service_id , 
+    template_id,
+    user_id ,
     template_params: {
         to_name: 'Harsh',
         message: message
         
     },
-    accessToken:'O342h4LQQ7DlYj1EaBP8K'
+    accessToken
 };
 
 fetch('https://api.emailjs.com/api/v1.0/email/send', {
